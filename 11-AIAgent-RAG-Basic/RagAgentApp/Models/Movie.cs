40376381422cf056ec060e5e07baffccef8b9a9c
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-10-25 09:38:46 -0400

    AgentRAG basic

---
+++ 11-AIAgent-RAG-Basic/RagAgentApp/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RagAgentApp.Models;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Plot { get; set; }
+    public decimal Rating { get; set; }
+    public int Year { get; set; }
+
+    internal string? GetTitleAndDetails()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ 11-AIAgent-RAG-Basic/RagAgentApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using OpenAI.Chat;
+using RagAgentApp;
+using RagAgentApp.Models;
+using System.Text.Json;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
+
+AzureOpenAIClient client = new AzureOpenAIClient(new Uri(LLMConfig.Endpoint),
+                        new System.ClientModel.ApiKeyCredential(LLMConfig.ApiKey));
+
+ChatClient chatClient = client.GetChatClient(LLMConfig.DeploymentOrModelId);
+
+string jsonMovies = await File.ReadAllTextAsync("mymovies.json");
+
+Movie[] moviesDataForRAG = JsonSerializer.Deserialize<Movie[]>(jsonMovies)!;
+
+ChatMessage question = new ChatMessage(ChatRole.User, "List 3 highest rated adventure movies (list their title, plot, year and rating ?");
+
+ChatClientAgent agent = chatClient.CreateAIAgent(instructions: "You are an expert on a set of fictious movies given to you. (don't have any idea about real world movies");
+
+Utils.WriteLineSuccess("Scenario 1: RAG with Azure OpenAI Service");
+List<ChatMessage> preLoadedEverythingChatMessages = [
+      new(ChatRole.Assistant, "Here are all the movies")
+    ];
+
+foreach(Movie movie in moviesDataForRAG)
+{
+    preLoadedEverythingChatMessages.Add(new ChatMessage(ChatRole.Assistant, movie.GetTitleAndDetails()));
+}
+
+preLoadedEverythingChatMessages.Add(question);
+AgentRunResponse response1 = await agent.RunAsync(preLoadedEverythingChatMessages);
+Console.WriteLine(response1);
+response1.Usage.OutputAsInformation();
+
+Console.Clear();
