5cb162cba5ee4882c53b37f25bf7fc96f7df7451
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-10-26 19:18:08 -0400

    Basic RAG used 1. Directly pass all the Json data to LLM and get the response 2. Use the Similarity Search and pass the top result to the LLM along with the question 3. Place the similarity search as a tool and perform tool calling in the AIAgent

--- 11-AIAgent-RAG-Basic/RagAgentApp/Program.cs
+++ 11-AIAgent-RAG-Basic/RagAgentApp/Program.cs
@@ -2,15 +2,18 @@
 using Azure.AI.OpenAI;
 using Azure.Search.Documents.Indexes;
 using Microsoft.Agents.AI;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.VectorData;
 using Microsoft.SemanticKernel.Connectors.AzureAISearch;
+using Microsoft.SemanticKernel.Connectors.CosmosNoSql;
 using Microsoft.SemanticKernel.Connectors.InMemory;
 using Microsoft.SemanticKernel.Connectors.SqlServer;
 using OpenAI;
 using OpenAI.Chat;
 using RagAgentApp;
 using RagAgentApp.Models;
+using System.Text;
 using System.Text.Json;
 using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
 
@@ -45,6 +48,7 @@ Utils.WriteLineInformation("************************************");
 Utils.WriteLineInformation($"- Input Tokens:{response1.Usage?.InputTokenCount}");
 Utils.WriteLineInformation($"- Output Tokens:{response1.Usage?.OutputTokenCount}" +
     $"({response1.Usage?.GetOutputTokensUsedForReasoning()} was used for reasoning)");
+
 Utils.Separator();
 
 Console.ReadLine();
@@ -59,9 +63,17 @@ InMemoryVectorStore vectorStore = new(new InMemoryVectorStoreOptions
     EmbeddingGenerator = embeddingGenerator
 });
 
-//AzureAISearchVectorStore vectorStoreAzSql = new SearchIndexClient(new Uri("azureAISearchEndpoint"),
-//    new AzureKeyCredential("azureAiSearchKey"));
-//SqlServerVectorStore vectorStoreFromSqlServer2025 = new ()
+//AzureAISearchVectorStore vectorStoreAzSql = new AzureAISearchVectorStore(
+//        new SearchIndexClient(new Uri("azureAISearchEndpoint"),
+//   new AzureKeyCredential("azureAiSearchKey")));
+
+//SqlServerVectorStore vectorStoreFromSqlServer2025 = new SqlServerVectorStore("connectionString");
+
+//CosmosNoSqlVectorStore vectorStoreCosmos = new CosmosNoSqlVectorStore("connectionString", "databasename",
+//    new CosmosClientOptions
+//    {
+//        UseSystemTextJsonSerializerWithOptions = JsonSerializerOptions.Default
+//    });
 
 InMemoryCollection<Guid, MovieVectorStoreRecord> collection = vectorStore.GetCollection<Guid, MovieVectorStoreRecord>("movies");
 await collection.EnsureCollectionExistsAsync();
@@ -85,7 +97,7 @@ Console.WriteLine("\rEmbedding movies: Done.. Lets ask the questions again using
 
 List<ChatMessage> ragPreloadedChatMessages = [ new ChatMessage(ChatRole.Assistant, "Here are the more relevant movies") ];
 
-await foreach (VectorSearchResult<MovieVectorStoreRecord> searchResult in collection.SearchAsync(question.Text, 10,
+await foreach (VectorSearchResult<MovieVectorStoreRecord> searchResult in collection.SearchAsync(question.Text, 5,
     new VectorSearchOptions<MovieVectorStoreRecord>
     {
         IncludeVectors = false
@@ -105,4 +117,57 @@ Utils.WriteLineInformation($"- Output Tokens:{response2.Usage?.OutputTokenCount}
     $"({response2.Usage?.GetOutputTokensUsedForReasoning()} was used for reasoning)");
 Utils.Separator();
 
+Console.ReadLine();
+//Console.Clear();
+
+Utils.WriteLineSuccess("Scenario 3: RAG with AIAgent Toolkit");
+SearchTool searchTool = new SearchTool(collection);
+AIAgent aiAgent = client.GetChatClient(LLMConfig.DeploymentOrModelId)
+                    .CreateAIAgent(instructions:"You are an expert on a set of fictious movies given to you. (don't have any idea about real world movies",
+                    tools: [AIFunctionFactory.Create(searchTool.SearchVectorStore)])
+                    .AsBuilder()
+                    .Use(FunctionCallingMiddleware)
+                    .Build();
+AgentRunResponse response3 = await aiAgent.RunAsync(question);
+Console.WriteLine(response3);
+Utils.WriteLineInformation("************************************");
+Utils.WriteLineInformation($"- Input Tokens:{response3.Usage?.InputTokenCount}");
+Utils.WriteLineInformation($"- Output Tokens:{response3.Usage?.OutputTokenCount}" +
+    $"({response3.Usage?.GetOutputTokensUsedForReasoning()} was used for reasoning)");
+
+Console.ReadLine();
+async ValueTask<object?> FunctionCallingMiddleware(
+    AIAgent callingAgent,
+    FunctionInvocationContext context, Func<FunctionInvocationContext, CancellationToken, ValueTask<object?>> next,
+    CancellationToken token)
+{
+    StringBuilder functionCallDetails = new();
+    // Log the function tool which is being called
+    functionCallDetails.Append($"Tool Call:'{context.Function.Name}'");
+    // Log the arguments if any when calling the tool
+    if (context.Arguments.Count > 0)
+    {
+        functionCallDetails.Append($" (Args: {string.Join(", ", context.Arguments.Select(kv => $"{kv.Key}:{kv.Value}"))})");
+    }
+    Utils.WriteLineInformation(functionCallDetails.ToString());
+    return await next(context, token);
+}
+
+class SearchTool(InMemoryCollection<Guid, MovieVectorStoreRecord> collection)
+{
+    public async Task<List<string>> SearchVectorStore(string question)
+    {
+        List<string> result = [];
+        await foreach (VectorSearchResult<MovieVectorStoreRecord> searchResult in collection.SearchAsync(question, 5,
+            new VectorSearchOptions<MovieVectorStoreRecord>
+            {
+                IncludeVectors = false
+            }))
+        {
+            MovieVectorStoreRecord record = searchResult.Record;
+            result.Add(record.GetTitleAndDetails());
+        }
+        return result;
+    }
+}
 
