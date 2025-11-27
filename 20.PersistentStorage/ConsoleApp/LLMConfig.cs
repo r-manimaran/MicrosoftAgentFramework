07d90475e464b6a607002a37b4630f7fd03ea75c
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2025-11-27 16:33:17 -0500

    Persistent Storage with Mongodb

---
+++ 20.PersistentStorage/ConsoleApp/LLMConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp;
+
+public class LLMConfig
+{
+    private static readonly IConfiguration configuration;
+    static LLMConfig()
+    {
+        configuration = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+            .AddUserSecrets<LLMConfig>()
+            .Build();
+    }
+    public static string DeploymentOrModelId => configuration["AzureAI:ModelId"] ?? "gpt-4o";
+    public static string Endpoint => configuration["AzureAI:Endpoint"] ?? throw new ArgumentNullException("AzureAI:Endpoint");
+    public static string ApiKey => configuration["AzureAI:ApiKey"] ?? throw new ArgumentNullException("AzureAI:Api");
+    public static string GitHubToken => configuration["GitHub:Token"] ?? throw new ArgumentNullException("GitHub:Token");
+
+    public static string DocumentDBUserName => configuration["Documentdb:Username"] ?? throw new ArgumentNullException("Documentdb:Username");
+    public static string DocumentDBPassword => configuration["Documentdb:Password"] ?? throw new ArgumentNullException("Documentdb:Password");
+
+}
---
+++ 20.PersistentStorage/ConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using Azure.AI.OpenAI;
+using ConsoleApp;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using OpenAI;
+using System.Text.Json;
+
+string host = "localhost";
+int port = 10260;
+
+AzureOpenAIClient client = new(new Uri(LLMConfig.Endpoint), new System.ClientModel.ApiKeyCredential(LLMConfig.ApiKey));
+AIAgent agent = client.GetChatClient(LLMConfig.DeploymentOrModelId).CreateAIAgent();
+//AgentRunResponse response = await agent.RunAsync("What's the capital of India.?");
+//Console.WriteLine(response);
+//Console.ReadLine();
+
+AgentThread thread = agent.GetNewThread();
+
+const bool optionToResume = true;
+//if (optionToResume)
+//{
+//    // Resume from previous conversation
+//    thread = await AgentThreadPersistence.ResumeChatIfRequestedAsync(agent);
+//}
+
+var credentialDb = MongoCredential.CreateCredential("admin", LLMConfig.DocumentDBUserName, LLMConfig.DocumentDBPassword);
+MongoClientSettings settings = new()
+{
+    Credential = credentialDb,
+    Server = new MongoServerAddress(host, port),
+    UseTls = true,
+    AllowInsecureTls = true,
+};
+try
+{
+
+    using MongoClient mongoclient = new(settings);
+    var db = mongoclient.GetDatabase("ThreadData");
+    var collection = db.GetCollection<BsonDocument>("ThreadsCollection");
+    while (true)
+    {
+        Console.Write("User > ");
+        string? userInput = Console.ReadLine();
+        if (string.IsNullOrEmpty(userInput))
+        {
+            Utils.WriteLineError("Empty input, exiting.");
+            break;
+        }
+        ChatMessage message = new ChatMessage(ChatRole.User, userInput);
+
+        await foreach (AgentRunResponseUpdate update in agent.RunStreamingAsync(message, thread))
+        {
+            Console.Write(update);
+        }
+        JsonElement serializeThread = thread.Serialize();
+        var bsonDoc = BsonDocument.Parse(serializeThread.GetRawText());
+        await collection.InsertOneAsync(bsonDoc);
+        Console.Write($"{serializeThread.GetRawText()}");
+
+        Console.WriteLine();
+        Console.WriteLine(string.Empty.PadLeft(50, '*'));
+        Console.WriteLine();
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
