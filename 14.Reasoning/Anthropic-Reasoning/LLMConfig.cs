f92bee5f28d6e3a60089d7a1d8880336d96e9c63
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2026-02-15 19:11:55 -0500

    Using reasoning with Anthropic

---
+++ 14.Reasoning/Anthropic-Reasoning/LLMConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anthropic_Reasoning;
+
+public class LLMConfig
+{
+    private static readonly IConfiguration configuration;
+    static LLMConfig()
+    {
+        configuration = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+            .AddUserSecrets<LLMConfig>()
+            .Build();
+    }
+  
+    public static string ApiKey => configuration["Anthropic:ApiKey"] ?? throw new ArgumentNullException("Anthropic:ApiKey");
+}
---
+++ 14.Reasoning/Anthropic-Reasoning/Program.cs
@@ -0,0 +1,35 @@
+﻿using Anthropic;
+using Anthropic.Core;
+using Anthropic_Reasoning;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+
+string question = "What is the capital of the state where the Marina beach is located" +
+                   " and what is the population of that city? (Answer back in max 3 words)";
+
+Console.Clear();
+Utils.WriteLineSuccess("Baseline (No thiniking)");
+await Baseline();
+
+Console.ReadLine();
+
+
+
+async Task Baseline()
+{
+    AnthropicClient client = new(new ClientOptions
+    {
+        ApiKey = LLMConfig.ApiKey
+    });
+    ChatClientAgent agent = new(client.AsIChatClient("claude-haiku-4-5-20251001"),
+        new ChatClientAgentOptions
+        {
+           ChatOptions = new ChatOptions
+           {
+               MaxOutputTokens = 10000
+           }            
+        });
+    AgentResponse response = await agent.RunAsync(question);
+    Console.WriteLine(response);
+    response?.Usage.OutputAsInformation();
+}
