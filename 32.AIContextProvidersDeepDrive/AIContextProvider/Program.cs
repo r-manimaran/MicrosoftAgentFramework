83f1be4171cd6baef2a40f7488a48ffdcc1b7009
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2026-03-18 19:50:50 -0400

    Initial commit

---
+++ 32.AIContextProvidersDeepDrive/AIContextProvider/Program.cs
@@ -0,0 +1,40 @@
+﻿using AgentFrameworkToolkit;
+using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using OpenAI.Chat;
+using Shared;
+using System.ClientModel;
+using System.ClientModel.Primitives;
+
+Utils.Init("AIContextProviders");
+(Uri endpoint, ApiKeyCredential apiKey) = SecretsManager.GetAzureOpenAICredentials();
+
+AzureOpenAIClient client = new(endpoint, apiKey, new AzureOpenAIClientOptions
+{
+    Transport = new HttpClientPipelineTransport(new HttpClient(new RawCallDetailsHttpHandler(r=>r.RequestUrl="")))
+});
+
+AIAgent agent = client.GetChatClient("gpt-4o-mini")
+    .AsAIAgent(new ChatClientAgentOptions
+    {
+        ChatOptions = new Microsoft.Extensions.AI.ChatOptions
+        {
+            Instructions = "Prefix all messages with 👌👌👌"
+        },
+        AIContextProviders = [
+
+            ]
+    }).AsBuilder()
+    .Build();
+
+AgentSession session = await agent.CreateSessionAsync();
+while  (true)
+{
+    Console.Write(">");
+    string input = Console.ReadLine() ?? "";
+    AgentResponse response = await agent.RunAsync(input,session);
+    Utils.Separator();
+    Utils.Red("Final Response:");
+    Console.WriteLine(response);
+    Utils.Separator();
+}
---
+++ 32.AIContextProvidersDeepDrive/Shared/Extensions/UsageDetailsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Extensions;
+
+public static class UsageDetailsExtensions
+{
+   
+    public static void OutputAsInformation(this UsageDetails? usageDetails)
+    {
+        if (usageDetails == null)
+        {
+            return;
+        }
+
+        Utils.Gray($"- Input Tokens: {usageDetails.InputTokenCount}");
+        var output = $"- Output Tokens: {usageDetails.OutputTokenCount} " + $"({usageDetails.ReasoningTokenCount ?? 0} was used for reasoning)";
+        Utils.Gray(output);
+    }
+}
