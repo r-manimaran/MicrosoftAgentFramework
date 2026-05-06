c1a239a014ef59aab4707ae42c3ba06281d79d01
Manimaran Ramakrishnan <r.manimaran@yahoo.co.in>
2026-05-05 20:53:26 -0400

    Used DI to register the service

--- 30.AgentSkills/ClassbasedSkills/Program.cs
+++ 30.AgentSkills/ClassbasedSkills/Program.cs
@@ -2,13 +2,26 @@
 using ClassbasedSkills;
 using Microsoft.Agents.AI;
 using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
 using OpenAI.Chat;
 using Shared;
 
 #pragma warning disable MAAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-var skill = new UnitConverterSkill();
-var skillProvider = new AgentSkillsProvider(skill);
+// Option 1: We can use directly like below
+// var skill = new UnitConverterSkill();
+
+// Option 2: We can use the DI like below
+ServiceCollection servicesCollection = new();
+servicesCollection.AddSingleton<UnitConverterSkill>();
+IServiceProvider sp = servicesCollection.BuildServiceProvider();
+
+// For Option 1: use like below
+// var skillProvider = new AgentSkillsProvider(skill);
+
+// For option 2: Use DI and get the Class Skill
+var unitConverterSkill = sp.GetRequiredService<UnitConverterSkill>();
+var skillProvider = new AgentSkillsProvider(unitConverterSkill);
 
 AzureOpenAIClient client = Utils.GetAzureOpenAIClient(showRawCall: true);
 
---
+++ 30.AgentSkills/ClassbasedSkills/UnitConverterService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassbasedSkills;
+
+public class UnitConverterService
+{
+}
